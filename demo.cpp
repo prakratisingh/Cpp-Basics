bb2a8a500c226627997bfb3365c7a3c82eb479d4
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2022-08-15 21:35:03 +0530

    Basic input output

---
+++ demo.cpp
@@ -0,0 +1,16 @@
+#include<iostream> //preposessor directive (used to include files)
+using namespace std;
+int main() //Execution starts from main function. (Mandatory for every program)
+{
+    cout<<"Hello"<<endl;
+    cout<<"Hello World\n";
+    // return 0; //exit status of a function
+    //INPUT FROM USER
+    int amount1;
+    cin>>amount1;
+    int amount2;
+    cin>>amount2;
+    int sum = amount1+amount2;
+    cout<<sum<<endl;
+    return 0;
+}
