9b4adc02669820b9e81f69d7ea7f556ae6a423e3
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2022-08-16 19:14:32 +0530

    2. Loops

---
+++ loops.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+//for loop
+// int main()
+// {
+//     int n,sum=0;
+//     cin>>n;
+//     for(int i=0;i<=n;i++)
+//     {
+//         sum=sum+i;
+//     }
+//     cout<<sum<<endl;
+//     return 0;
+// }
+//while loop - terminate as soon as user enters -ve number
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     while(n>0)
+//     {
+//         cout<<n<<endl;
+//         cin>>n;
+//     }
+//     return 0;
+// }
+
+//do-while loop - first statements will be executed then condition specified
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     do{
+//         cout<<n<<endl;
+//         cin>>n;
+//     }
+//     while(n>0);
+//     return 0;
+// }
+
