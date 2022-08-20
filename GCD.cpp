0c16dc1a5ab5915fd234fdcac284ab75c43d6954
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2022-08-20 17:05:44 +0530

    12. GCD of two numbers

---
+++ GCD.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    int gcd=1;
+    cin>>n1>>n2;
+    int mn = min(n1,n2);
+    for(int i=1;i<=mn;i++)
+    {
+        if(n1%i==0 && n2%i==0)
+        {
+            gcd = i;
+        }
+    }
+    cout<<"GCD:  "<<gcd<<endl;
+    return 0;
+}
